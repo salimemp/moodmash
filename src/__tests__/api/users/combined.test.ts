3db167f8938690153db6528c041db02c105ff36b
salimemp <salimemp@yahoo.in>
2025-03-21 10:26:05 +0300

    Add coverage tests for users/[id]/public-key.ts endpoint

---
+++ src/__tests__/api/users/combined.test.ts
@@ -0,0 +1,10 @@
+/**
+ * Combined tests for Users API endpoints
+ * 
+ * This file imports all user-related test files to run them together
+ */
+
+// Import the user endpoint test files
+import './[id]/public-key.test';
+
+// This is just a wrapper file to run all user-related tests together 
