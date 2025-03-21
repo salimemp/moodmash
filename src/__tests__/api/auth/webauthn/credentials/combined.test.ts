c4e4eafb0e6cd894e7c71e037403f6004a64f679
salimemp <salimemp@yahoo.in>
2025-03-21 10:17:31 +0300

    Update combined test to include id-direct test

--- src/__tests__/api/auth/webauthn/credentials/combined.test.ts
+++ src/__tests__/api/auth/webauthn/credentials/combined.test.ts
@@ -1,5 +1,12 @@
+/**
+ * Combined tests for WebAuthn credentials API
+ * 
+ * This file imports all test files to run them together
+ */
+
 // Import the existing test files
 import './[id].test';
+import './id-direct.test';
 import './index.test';
 
-// This is just a wrapper file to run both tests together 
+// This is just a wrapper file to run all tests together 
